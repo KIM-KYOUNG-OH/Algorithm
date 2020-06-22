1e96521fd64c4417138fb282aae03e604ea9bfba
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2020-06-22 23:19:42 +0900

    알고리즘

---
+++ src/basic/P10950.java
@@ -0,0 +1,16 @@
+package basic;
+
+import java.util.Scanner;
+
+public class P10950 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		line
+		for(int i = 0; i < )
+		System.out.println(A+B);
+	}
+
+}
