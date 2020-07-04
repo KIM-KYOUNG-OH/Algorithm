cff2e456992a2903d551eee5b42b56643d440edb
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2020-07-04 14:59:38 +0900

    알고리즘

--- src/basic/P15552.java
+++ src/basic/P15552.java
@@ -1,14 +1,31 @@
 package basic;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class P15552 {
 
-	public static void main(String[] args) {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(bf.readLine());
-		StringTokenizer st = new StringTokenizer(s);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		String s = bf.readLine(); //String형
+//		StringTokenizer st = new StringTokenizer(s); //공백단위 데이터 가공
+//		String array[] = s.split(" "); //공백처리
+		int n = Integer.parseInt(br.readLine()); //int형변환
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i<n; i++) {
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			bw.write((a+b)+"\n");
+		}
+		
+		bw.flush();
+		bw.close();
 	}
 
 }
