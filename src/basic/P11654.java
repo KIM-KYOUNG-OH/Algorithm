755873eb65217595e3b59cf8b590ee2b8cae5eac
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2020-07-22 23:31:20 +0900

    7/22

---
+++ src/basic/P11654.java
@@ -0,0 +1,14 @@
+package basic;
+
+import java.util.Scanner;
+
+public class P11654 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String ascii = scanner.next();
+		int result = ascii.charAt(0);
+		System.out.println(result);
+	}
+
+}
